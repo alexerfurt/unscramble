7cfa2830a24403b2583af6238a7a8cbce4c542e2
Alex Erfurt <erfurtalex@gmail.com>
2019-06-06 21:34:51 +0100

    Adding comments to code

--- Task0.py
+++ Task0.py
@@ -11,7 +11,10 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+# Getting the index of the last row of the calls list
 last_idx = len(calls)-1
+
+# Printing the messages
 print(f"First record of texts, {texts[0][0]} texts {texts[0][1]} at time {texts[0][2]}")
 print(f"Last record of calls, {calls[last_idx][0]} calls {calls[last_idx][1]} at time {calls[last_idx][2]}, lasting {calls[last_idx][3]} seconds")
 
